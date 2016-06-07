ecd480e9584f4f6749e0aadfe51cc81e2a33e407
1fanfan1 <i.am.fanfan@yandex.ru>
2016-06-07 17:44:55 +0300

    Add files via upload

---
+++ Sphere.h
@@ -0,0 +1,16 @@
+/*FILE NAME: Sphere.h
+ *PROGRAMMER: AS5
+ *DATE: 07.06.2016
+ *PURPOSE: WinApi
+*/
+
+#ifndef __SPHERE_H_
+#define __SPHERE_H_
+
+#include <windows.h>
+
+VOID DrawSphere( HDC hDC, INT Xc, INT Yc );
+
+#endif /* __SPHERE_H_ */
+
+/* END OF 'Sphere.h' FILE */
---
+++ T06SPHR.C
@@ -0,0 +1,160 @@
+  /*FILE NAME: T06SPHR.C
+ *PROGRAMMER: AS5
+ *DATE: 07.06.2016
+ *PURPOSE: WinApi
+*/
+
+#include <windows.h>
+
+#include "Sphere.h"
+/* forward references */
+LRESULT CALLBACK MyWinFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam );
+VOID FlipFullScreen( HWND hWnd );
+
+/* Begin of 'WinMain' function*/
+INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, CHAR *CmdLine, INT ShowCmd )
+{
+  WNDCLASS wc;
+  HWND hWnd;
+  MSG msg;
+
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hCursor = LoadCursor(NULL, IDC_HAND);
+  wc.hIcon = LoadIcon(NULL, IDI_EXCLAMATION);
+  wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+  wc.hInstance = hInstance;
+  wc.lpszClassName = "My Window Class";
+  wc.lpszMenuName = NULL;
+  wc.lpfnWndProc = MyWinFunc;
+
+  if (!RegisterClass(&wc))
+  {
+    MessageBox(NULL, "ERROR REGISTER WINDOW CLASS", "ERROR", MB_OK);
+    return 0;
+  }
+
+  hWnd = CreateWindow("My Window Class",         
+    "© Shvetz Corporation, 2016",
+    WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT, CW_USEDEFAULT,
+    CW_USEDEFAULT, CW_USEDEFAULT,
+    NULL, NULL, hInstance, NULL);
+
+  ShowWindow(hWnd, SW_SHOWNORMAL);
+  UpdateWindow(hWnd);
+
+  while (GetMessage(&msg, NULL, 0, 0))
+    DispatchMessage(&msg);
+
+  return 0;
+}/* Begin of 'WinMain' function*/
+
+/* Begin of 'MyWinFunc' function*/
+LRESULT CALLBACK MyWinFunc( HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam )
+{
+  HDC hDC;
+  PAINTSTRUCT ps;
+  INT i = 0, j = 0, k = 0, l = 0;
+
+  static INT w, h;
+  static HDC hMemDC;
+  static HBITMAP hBm;
+  static BITMAP bm;
+  MINMAXINFO *MinMax;
+
+  switch (Msg)
+  {
+  case WM_GETMINMAXINFO:
+    MinMax = (MINMAXINFO *)lParam;
+    MinMax->ptMaxTrackSize.y =
+      GetSystemMetrics(SM_CYMAXTRACK) +
+      GetSystemMetrics(SM_CYCAPTION) +
+      GetSystemMetrics(SM_CYMENU) +
+      GetSystemMetrics(SM_CYBORDER) * 2;
+    return 0;
+  case WM_CREATE:
+    SetTimer(hWnd, 15, 10, NULL);
+    hDC = GetDC(hWnd);
+    hMemDC = CreateCompatibleDC(hDC);
+    ReleaseDC(hWnd, hDC);
+    return 0;
+  case WM_SIZE:
+    w = LOWORD(lParam);
+    h = HIWORD(lParam);
+    if (hBm != NULL)
+      DeleteObject(hBm);
+    hDC = GetDC(hWnd);
+    hBm = CreateCompatibleBitmap(hDC, w, h);
+    ReleaseDC(hWnd, hDC);
+    SelectObject(hMemDC, hBm);
+    SendMessage(hWnd, WM_TIMER, 0, 0);
+    return 0;
+  case WM_TIMER:
+    Rectangle(hMemDC, 0, 0, w + 1, h + 1);
+    DrawSphere(hMemDC, w / 2, h / 2);
+    InvalidateRect(hWnd, NULL, FALSE);
+    return 0;
+  case WM_PAINT:
+    hDC = BeginPaint(hWnd, &ps);
+    BitBlt(hDC, 0, 0, w, h, hMemDC, 0, 0, SRCCOPY);
+    EndPaint(hWnd, &ps);
+    return 0;
+  case WM_KEYDOWN:
+    if (LOWORD(wParam) == 'F')
+      FlipFullScreen(hWnd);
+    else if (LOWORD(wParam) == VK_ESCAPE)
+      DestroyWindow(hWnd);
+    return 0;
+  case WM_DESTROY:
+    KillTimer(hWnd, 15);
+    DeleteDC(hMemDC);
+    DeleteObject(hBm);
+    PostQuitMessage(0);
+    return 0;
+  }
+  return DefWindowProc(hWnd, Msg, wParam, lParam);
+}/*  End of 'MyWinFunc' function*/
+
+/* Set/reset full screen mode function */
+VOID FlipFullScreen( HWND hWnd )
+{
+  static BOOL IsFullScreen = FALSE;
+  static RECT SaveRect;
+
+  if (IsFullScreen)
+  {
+    /* restore window size */
+    SetWindowPos(hWnd, HWND_TOP,
+      SaveRect.left, SaveRect.top,
+      SaveRect.right - SaveRect.left, SaveRect.bottom - SaveRect.top,
+      SWP_NOOWNERZORDER);
+  }
+  else
+  {
+    /* Set full screen size to window */
+    HMONITOR hmon;
+    MONITORINFOEX moninfo;                       
+    RECT rc;
+
+    /* Store window old size */
+    GetWindowRect(hWnd, &SaveRect);
+
+    /* Get nearest monitor */
+    hmon = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
+
+    /* Obtain monitor info */
+    moninfo.cbSize = sizeof(moninfo);
+    GetMonitorInfo(hmon, (MONITORINFO *)&moninfo);
+
+    /* Set window new size */
+    rc = moninfo.rcMonitor;
+    AdjustWindowRect(&rc, GetWindowLong(hWnd, GWL_STYLE), FALSE);
+
+    SetWindowPos(hWnd, HWND_TOPMOST,
+      rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top,
+      SWP_NOOWNERZORDER);
+  }
+  IsFullScreen = !IsFullScreen;
+} /* End of 'FlipFullScreen' function */
