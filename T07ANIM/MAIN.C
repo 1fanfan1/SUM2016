0f595103400135b46e1574b396846700e71b6a53
1fanfan1 <i.am.fanfan@yandex.ru>
2016-06-08 18:39:42 +0400

    Add files via upload

---
+++ T07ANIM/MAIN.C
@@ -0,0 +1,17 @@
+/*FILE NAME: MAIN.C
+ *PROGRAMMER: AS5
+ *DATE: 08.06.2016
+ *PURPOSE: WinApi
+*/
+
+#include <windows.h>
+
+#include "VEC.H"
+
+/* Begin of 'WinMain' function*/
+INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, CHAR *CmdLine, INT ShowCmd )
+{
+  VEC V;
+
+  return 0;
+}/* Begin of 'WinMain' function*/
