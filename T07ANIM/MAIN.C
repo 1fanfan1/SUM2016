0f663994cc82d14ed6861a30f71db29bc6b99535
1fanfan1 <i.am.fanfan@yandex.ru>
2016-06-09 17:09:11 +0400

    Add files via upload
    All function have been modified and checked

--- T07ANIM/MAIN.C
+++ T07ANIM/MAIN.C
@@ -8,10 +8,29 @@
 
 #include "VEC.H"
 
-/* Begin of 'WinMain' function*/
+/* Begin of 'WinMain' function */
 INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, CHAR *CmdLine, INT ShowCmd )
 {
-  VEC V;
+  MATR m =
+  {{
+    {2, 0, 0, 0},
+    {1, 2, 0, 0},
+    {1, 1, 2, 0},
+    {1, 1, 1, 2}
+  }};
+  DBL g;
+
+  m = MatrIdentity();
+  m = MatrTranslate(VecSet(1, 2, 3));
+  m = MatrScale(VecSet(1, 2, 3));
+  m = MatrRotateX(30);
+  m = MatrRotateY(30);
+  m = MatrRotateZ(30);
+  m = MatrRotate(30, VecSet(1, 2, 3));
+  m = MatrMulMatr(MatrRotateX(90), MatrScale(VecSet(2, 2, 2)));
+  m = MatrInverse(m);
+  g = MatrDeterm(m);
+  m = MatrTranspose(m);
 
   return 0;
-}/* Begin of 'WinMain' function*/
+}/* Begin of 'WinMain' function */
